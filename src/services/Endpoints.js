fd362817ae3a3725302c5e31f217e3bcf21b6d02
Otieno Rowland <r.otieno@kokonetworks.com>
2021-04-20 14:56:22 +0300

    fix: use correct login endpoint pointing to open cv server

--- src/services/Endpoints.js
+++ src/services/Endpoints.js
@@ -1,4 +1,4 @@
-const DOMAIN = 'https://g2fa.azurewebsites.net/api' //IF prod XWF
+const DOMAIN = 'https://192.168.0.107/api' //IF prod XWF
 
 export default {
   'LOGIN': [DOMAIN, 'login'].join('/'),
